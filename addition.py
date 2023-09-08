0ce6776dede56a8166547fc84c018612acfc6ba5
Ege <53268284+Egosdev@users.noreply.github.com>
2023-09-08 21:05:37 +0300

    addition settings and config

--- addition.py
+++ addition.py
@@ -1,6 +1,7 @@
 import random
 import time
 import os
+import yaml
 
 class COLOR:
     HEADER = '\033[95m'
@@ -12,14 +13,15 @@ class COLOR:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-    
+
 #SETTINGS
 QUESTIONS_COUNT = 10
 NUMBER_OF_DIGITS = [2,2]
 IS_DIGITS_INTERVAL = False
-IS_DIGITS_RANDOM = False
+IS_DIGITS_DIFFERENT = False
 MEMORIZE_MODE = False
 MEMORIZE_SECONDS = 1.2
+CUMULATIVE_MODE = False
 
 class Status_Object:
   def __init__(self, question = "", seconds = 0, first_try = -1):
@@ -28,12 +30,8 @@ class Status_Object:
     self.first_try = first_try
     
 status_array = []
-current_question = 0
 
-for i in range (QUESTIONS_COUNT):
-    status_array.append(Status_Object())
-
-def display_status_bar():
+def display_status_bar(current_question = QUESTIONS_COUNT):
     os.system('cls')
     print("\n   ", end = " ")
     for i in range (QUESTIONS_COUNT):
@@ -45,7 +43,9 @@ def display_status_bar():
             print(f"{COLOR.OKGREEN}█{COLOR.ENDC}", end = " ")
         else:
             print(f"{COLOR.WARNING}█{COLOR.ENDC}", end = " ")
-    print(" " * (15 - (i % 10)), f" < [{current_question}/{QUESTIONS_COUNT}] Questions\n\n")
+    print(" " * (15 - (i % 10)), f" < [{current_question}/{QUESTIONS_COUNT}] Questions\n")
+    if (MEMORIZE_MODE):
+        print(f"    {COLOR.OKGREEN}a{COLOR.ENDC} or {COLOR.OKGREEN}A{COLOR.ENDC} | Show question again\n")
 
 def result_statistics():
     display_status_bar()
@@ -69,7 +69,7 @@ def result_statistics():
     avg_time = round(total_time / QUESTIONS_COUNT, 2)
     total_time = round(total_time, 2)
     
-    print(f"{COLOR.OKGREEN} █ GREEN{COLOR.ENDC} squares means solved on {COLOR.UNDERLINE}first try{COLOR.ENDC}")
+    print(f"{COLOR.OKGREEN} █ GREEN{COLOR.ENDC} squares means solved on {COLOR.UNDERLINE}first try{COLOR.ENDC},")
     print(f"{COLOR.WARNING} █ YELLOW{COLOR.ENDC} squares means {COLOR.UNDERLINE}not{COLOR.ENDC} solved at once\n")
     print(f"   - -*- - {COLOR.OKBLUE}STATISTICS{COLOR.ENDC} - -*- -\n")
     print(f" {COLOR.OKBLUE} * Total time{COLOR.ENDC}                for {COLOR.UNDERLINE}{QUESTIONS_COUNT}{COLOR.ENDC} questions {COLOR.OKCYAN}{total_time} sec{COLOR.ENDC}")
@@ -95,68 +95,93 @@ def result_statistics_details(_avg_time):
         count += 1
     print(" ")
     
-    
-#Start game
+def start_game():
+    with open('config.yaml', 'r') as file:
+        config_data = yaml.safe_load(file)
+    global QUESTIONS_COUNT 
+    global NUMBER_OF_DIGITS 
+    global IS_DIGITS_INTERVAL 
+    global IS_DIGITS_DIFFERENT
+    global MEMORIZE_MODE 
+    global MEMORIZE_SECONDS 
+    global CUMULATIVE_MODE 
+    QUESTIONS_COUNT = config_data['addition_settings']['questions_count']
+    NUMBER_OF_DIGITS = config_data['addition_settings']['number_of_digits']
+    IS_DIGITS_INTERVAL = config_data['addition_settings']['is_digit_interval']
+    IS_DIGITS_DIFFERENT = config_data['addition_settings']['is_digit_different']
+    MEMORIZE_MODE = config_data['addition_settings']['memorize_mode']
+    MEMORIZE_SECONDS = config_data['addition_settings']['memorize_seconds']
+    CUMULATIVE_MODE = config_data['addition_settings']['cumulative_mode']
+    status_array.clear()
+    current_question = 0
+    for i in range (QUESTIONS_COUNT):
+        status_array.append(Status_Object())
 
-for i in range (QUESTIONS_COUNT):
-    current_question = i+1
-    display_status_bar()
-    if (IS_DIGITS_INTERVAL):
-        if (IS_DIGITS_RANDOM):
-            random_digits_one = random.randint(NUMBER_OF_DIGITS[0],NUMBER_OF_DIGITS[1])
-            random_digits_two = random.randint(NUMBER_OF_DIGITS[0],NUMBER_OF_DIGITS[1])
-            rn_one = random.randint((10 ** (random_digits_one-1)), (10 ** (random_digits_one))-1)
-            rn_two = random.randint((10 ** (random_digits_two-1)), (10 ** (random_digits_two))-1)
+    cumulative_sum = 0
+    for i in range (QUESTIONS_COUNT):
+        current_question = i+1
+        display_status_bar(current_question)
+        if (IS_DIGITS_INTERVAL):
+            if (IS_DIGITS_DIFFERENT):
+                random_digits_one = random.randint(NUMBER_OF_DIGITS[0],NUMBER_OF_DIGITS[1])
+                random_digits_two = random.randint(NUMBER_OF_DIGITS[0],NUMBER_OF_DIGITS[1])
+                rn_one = random.randint((10 ** (random_digits_one-1)), (10 ** (random_digits_one))-1)
+                rn_two = random.randint((10 ** (random_digits_two-1)), (10 ** (random_digits_two))-1)
+            else:
+                random_digits = random.randint(NUMBER_OF_DIGITS[0],NUMBER_OF_DIGITS[1])
+                rn_one = random.randint((10 ** (random_digits-1)), (10 ** (random_digits))-1)
+                rn_two = random.randint((10 ** (random_digits-1)), (10 ** (random_digits))-1)
         else:
-            random_digits = random.randint(NUMBER_OF_DIGITS[0],NUMBER_OF_DIGITS[1])
-            rn_one = random.randint((10 ** (random_digits-1)), (10 ** (random_digits))-1)
-            rn_two = random.randint((10 ** (random_digits-1)), (10 ** (random_digits))-1)
-    else:
-        rn_one = random.randint((10 ** (NUMBER_OF_DIGITS[0]-1)), (10 ** (NUMBER_OF_DIGITS[0]))-1)
-        rn_two = random.randint((10 ** (NUMBER_OF_DIGITS[0]-1)), (10 ** (NUMBER_OF_DIGITS[0]))-1)
-    rn_sum = rn_one + rn_two
-    start_time = time.time()
-    rn_input_info = f"{str(rn_one)} + {str(rn_two)}"
-    status_array[i].question = rn_input_info
-    if (MEMORIZE_MODE):
-        print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}]  {rn_input_info} : ")
-        print("        Memorize!")
-        time.sleep(MEMORIZE_SECONDS)
-        os.system('cls')
-        display_status_bar()
-        print(f"       {COLOR.OKBLUE}a{COLOR.ENDC} or {COLOR.OKBLUE}A{COLOR.ENDC} | Show question again")
-    while True:
+            rn_one = random.randint((10 ** (NUMBER_OF_DIGITS[0]-1)), (10 ** (NUMBER_OF_DIGITS[0]))-1)
+            rn_two = random.randint((10 ** (NUMBER_OF_DIGITS[0]-1)), (10 ** (NUMBER_OF_DIGITS[0]))-1)
+        
+        if (CUMULATIVE_MODE and cumulative_sum != 0):
+            rn_one = cumulative_sum
+        rn_sum = rn_one + rn_two
+        start_time = time.time()
+        rn_input_info = f"{str(rn_one)} + {str(rn_two)}"
+        status_array[i].question = rn_input_info
         if (MEMORIZE_MODE):
-            print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}] Your answer: ", end = "")
-        else:
-            print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}]  {rn_input_info} : ", end = "")
-        rn_user_input = input("")
-        try:
-            if (MEMORIZE_MODE and (rn_user_input == "a" or rn_user_input == "A")):
-                status_array[i].first_try = False;
-                os.system('cls')
-                display_status_bar()
-                print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}]  {rn_input_info} : ")
-                print("        Memorize!")
-                time.sleep(MEMORIZE_SECONDS)
-                os.system('cls')
-                display_status_bar()
-                print(f"       {COLOR.OKBLUE}a{COLOR.ENDC} or {COLOR.OKBLUE}A{COLOR.ENDC} | Show question again")
+            if (CUMULATIVE_MODE and cumulative_sum != 0):
+                rn_input_info = f"{COLOR.FAIL}...{COLOR.ENDC} + {str(rn_two)}"
+            print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}][{COLOR.OKBLUE}M{COLOR.ENDC}]  {rn_input_info} : ")
+            print("        Memorize!")
+            time.sleep(MEMORIZE_SECONDS)
+            os.system('cls')
+            display_status_bar(current_question)
+        cumulative_sum = rn_sum
+        while True:
+            if (MEMORIZE_MODE):
+                print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}][{COLOR.OKBLUE}M{COLOR.ENDC}] Your answer: ", end = "")
+            else:
+                print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}]  {rn_input_info} : ", end = "")
+            rn_user_input = input("")
+            try:
+                if (MEMORIZE_MODE and (rn_user_input == "a" or rn_user_input == "A")):
+                    status_array[i].first_try = False;
+                    os.system('cls')
+                    display_status_bar(current_question)
+                    if (CUMULATIVE_MODE):
+                        rn_input_info = f"{COLOR.FAIL}...{str(rn_one)}{COLOR.ENDC} + {str(rn_two)}"
+                    print(f"  [{COLOR.OKBLUE}Q{i+1}{COLOR.ENDC}][{COLOR.OKBLUE}M{COLOR.ENDC}]  {rn_input_info} : ")
+                    print("        Memorize!")
+                    time.sleep(MEMORIZE_SECONDS)
+                    os.system('cls')
+                    display_status_bar(current_question)
+                    continue
+                rn_user_input = int(rn_user_input)
+            except ValueError:
+                print(f"  {COLOR.FAIL}Oops!{COLOR.ENDC} You should enter an {COLOR.FAIL}INTEGER{COLOR.ENDC}!\n")
                 continue
-            rn_user_input = int(rn_user_input)
-        except ValueError:
-            print(f" {COLOR.FAIL}Oops!{COLOR.ENDC} You have to type an {COLOR.FAIL}INTEGER{COLOR.ENDC}")
-            continue
-        if rn_user_input == rn_sum:
-            elapsed_time = round(time.time() - start_time, 2)
-            print(f" {COLOR.OKGREEN}CORRECT!{COLOR.ENDC}", elapsed_time, "seconds")
-            status_array[i].seconds = elapsed_time
-            if (status_array[i].first_try == -1):
-                status_array[i].first_try = True;
-            input(f" {COLOR.HEADER}>|{COLOR.ENDC} Press {COLOR.WARNING}enter{COLOR.ENDC} to continue...")
-            break
-        else:
-            print(f" {COLOR.FAIL}WRONG!{COLOR.ENDC}")
-            status_array[i].first_try = False;
-            
-result_statistics()
+            if rn_user_input == rn_sum:
+                elapsed_time = round(time.time() - start_time, 2)
+                print(f"  {COLOR.OKGREEN}CORRECT!{COLOR.ENDC}", elapsed_time, "seconds\n")
+                status_array[i].seconds = elapsed_time
+                if (status_array[i].first_try == -1):
+                    status_array[i].first_try = True;
+                input(f" {COLOR.HEADER}>|{COLOR.ENDC} Press {COLOR.WARNING}enter{COLOR.ENDC} to continue...")
+                break
+            else:
+                print(f"  {COLOR.FAIL}WRONG!{COLOR.ENDC}\n")
+                status_array[i].first_try = False;
+    result_statistics()
--- menu.py
+++ menu.py
@@ -2,9 +2,38 @@ import time
 import random
 import os
 import sys
-import keyboard
+import ctypes
+import addition
+import yaml
 
 os.system("")
+def set_font():
+    LF_FACESIZE = 32
+    STD_OUTPUT_HANDLE = -11
+
+    class COORD(ctypes.Structure):
+        _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+    class CONSOLE_FONT_INFOEX(ctypes.Structure):
+        _fields_ = [("cbSize", ctypes.c_ulong),
+                    ("nFont", ctypes.c_ulong),
+                    ("dwFontSize", COORD),
+                    ("FontFamily", ctypes.c_uint),
+                    ("FontWeight", ctypes.c_uint),
+                    ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+        
+    font = CONSOLE_FONT_INFOEX()
+    font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+    font.nFont = 12
+    font.dwFontSize.X = 32
+    font.dwFontSize.Y = 32
+    font.FontFamily = 54
+    font.FontWeight = 400
+    handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+    ctypes.windll.kernel32.SetCurrentConsoleFontEx(
+            handle, ctypes.c_long(False), ctypes.pointer(font))
+
+set_font()
 
 class COLOR:
     HEADER = '\033[95m'
@@ -88,23 +117,19 @@ while True:
     if (DISPLAY_SCREEN == 1):
         os.system('cls')
         prompt = ""
-        print("\n\n  [+] ADDITION QUIZ \n\n")
+        print("\n\n  + ADDITION QUIZ \n\n")
         print(f" Don't forget to {COLOR.OKBLUE}check the settings{COLOR.ENDC}, there are {COLOR.OKBLUE}different{COLOR.ENDC} modes.\n")
         print(f" {COLOR.FAIL}m{COLOR.ENDC} or {COLOR.FAIL}M{COLOR.ENDC} | {COLOR.FAIL}main menu{COLOR.ENDC}")
         print(f" {COLOR.OKGREEN}p{COLOR.ENDC} or {COLOR.OKGREEN}P{COLOR.ENDC} | {COLOR.OKGREEN}learning pathway{COLOR.ENDC} (0% progress)")
-        print(f" {COLOR.WARNING}r{COLOR.ENDC} or {COLOR.WARNING}R{COLOR.ENDC} | {COLOR.WARNING}ranked{COLOR.ENDC}")
-        print(f"    -> {COLOR.WARNING}s{COLOR.ENDC} or {COLOR.WARNING}S{COLOR.ENDC} | {COLOR.WARNING}ranked settings{COLOR.ENDC}\n")
-        prompt = input(f" {COLOR.HEADER}>|{COLOR.ENDC} Choose an action: ")
-        prompt = prompt.lower()
+        print(f" {COLOR.WARNING}r{COLOR.ENDC} or {COLOR.WARNING}R{COLOR.ENDC} | {COLOR.WARNING}ranked{COLOR.ENDC} >> {COLOR.WARNING}s{COLOR.ENDC} or {COLOR.WARNING}S{COLOR.ENDC} {COLOR.WARNING}ranked settings{COLOR.ENDC}\n")
+        prompt = input(f" {COLOR.HEADER}>|{COLOR.ENDC} Choose an action: ").lower()
         if (prompt == "r"):
             for i in range(3):
                 print(f"    {3-i}!")
                 time.sleep(1)
-            with open("addition.py", "r", encoding='utf-8') as addition_game:
-                addition_game_code = addition_game.read()
-                exec(addition_game_code)
-            while(prompt != "y" or prompt != "n"):
-                prompt = input(f" {COLOR.HEADER}>|{COLOR.ENDC} Do you wanna {COLOR.WARNING}play again?{COLOR.ENDC} (Y/N) ")
+            addition.start_game()
+            while (prompt != "y" or prompt != "n"):
+                prompt = input(f" {COLOR.HEADER}>|{COLOR.ENDC} Do you wanna {COLOR.WARNING}play again?{COLOR.ENDC} (Y/N) ").lower()
                 if (prompt == "n"):
                     change_screen(0)
                     break
@@ -112,9 +137,90 @@ while True:
                     os.system('cls')
                     break
         elif (prompt == "s"):
-            os.system('cls')
-            print("settings")
-            input(" press enter to continue... ")
+            with open('config.yaml') as file:
+                settings = yaml.safe_load(file)
+            while (prompt != "b"):
+                if (settings['addition_settings']['is_digit_interval']):
+                    number_of_digits_settings_text = "Interval "
+                    how_many_digits_settings_text = f"{settings['addition_settings']['number_of_digits'][0]}...{settings['addition_settings']['number_of_digits'][1]}"
+                    if (settings['addition_settings']['is_digit_different']):
+                        number_of_digits_settings_text += "-different "
+                        example_digits_settings_text = "(** + ***), (**** + *) etc."
+                    else:
+                        number_of_digits_settings_text += "-same      "
+                        example_digits_settings_text = "(** + **), (*** + ***) etc."
+                else:
+                    number_of_digits_settings_text = "Single Value        "
+                    how_many_digits_settings_text = f"{settings['addition_settings']['number_of_digits'][0]}"
+                    example_digits_settings_text = "("+(settings['addition_settings']['number_of_digits'][0] * "*") + " + " + (settings['addition_settings']['number_of_digits'][0] * "*")+")"
+                if (settings['addition_settings']['memorize_mode']):
+                    memorize_mode_settings_text = "on "
+                else:
+                    memorize_mode_settings_text = "off"
+                if (settings['addition_settings']['cumulative_mode']):
+                    cumulative_mode_settings_text = "on "
+                else:
+                    cumulative_mode_settings_text = "off"
+                os.system('cls')
+                print("\n\n  + ADDITION SETTINGS \n\n")
+                print(f" {COLOR.OKBLUE}[1]{COLOR.ENDC} Number of questions per run: {COLOR.WARNING}{settings['addition_settings']['questions_count']}{COLOR.ENDC}       (1...20)")
+                print(f" {COLOR.OKBLUE}[2]{COLOR.ENDC} Number of digits: {COLOR.WARNING}{number_of_digits_settings_text}{COLOR.ENDC}(single/interval) -[same/different]")
+                print(f"     {COLOR.WARNING}{how_many_digits_settings_text} digits{COLOR.ENDC} e.g. {example_digits_settings_text}")
+                print(f" {COLOR.OKBLUE}[3]{COLOR.ENDC} Memorize Mode: {COLOR.WARNING}{memorize_mode_settings_text}{COLOR.ENDC}                    (on/off)")
+                if (settings['addition_settings']['memorize_mode']):
+                    print(f"     {COLOR.WARNING}{settings['addition_settings']['memorize_seconds']} sec{COLOR.ENDC}")
+                print(f" {COLOR.OKBLUE}[4]{COLOR.ENDC} Cumulative Mode: {COLOR.WARNING}{cumulative_mode_settings_text}{COLOR.ENDC}                  (on/off)")
+                prompt = input(f"\n {COLOR.HEADER}>|{COLOR.ENDC} {COLOR.FAIL}b{COLOR.ENDC} or {COLOR.FAIL}B{COLOR.ENDC} to {COLOR.FAIL}back{COLOR.ENDC}, {COLOR.OKGREEN}[1-4]{COLOR.ENDC} to {COLOR.OKGREEN}select an action{COLOR.ENDC} for more info and adjustments: ").lower()
+                if (prompt == "1"):
+                    _input = -1
+                    while (_input > 20 or _input < 1):
+                        try:
+                            _input = int(input(f"\n Number of Questions\n > To specify how many consecutive questions will be asked,\n please {COLOR.WARNING}enter{COLOR.ENDC} a number {COLOR.WARNING}between 1 and 20{COLOR.ENDC}: "))
+                            if (_input >= 1 and _input <= 20):
+                                settings['addition_settings']['questions_count'] = _input
+                                break
+                            else:
+                                print(f"{COLOR.FAIL}Oops!{COLOR.ENDC} The number you entered should be {COLOR.FAIL}between 1 and 20{COLOR.ENDC}!\n")
+                        except ValueError:
+                            print(f"{COLOR.FAIL}Oops!{COLOR.ENDC} You should enter an {COLOR.FAIL}INTEGER{COLOR.ENDC}!\n")
+                elif (prompt == "2"):
+                    _input = ""
+                    while (_input != "single" or _input != "interval -same" or _input != "interval -different"):
+                        _input = input(f"\n Number of Digits\n If you want numbers with distinct digits to be generated within a range,\n  type: {COLOR.WARNING}interval -different{COLOR.ENDC} e.g. (123 + 6),(78 + 5453)...\n\n If you want numbers to be generated within a range, but you want the digits to be the same,\n  type: {COLOR.WARNING}interval -same{COLOR.ENDC} e.g. (12 + 39),(325 + 112)...\n\n If you want only numbers with the exact number of digits you desire to be generated,\n  type: {COLOR.WARNING}single{COLOR.ENDC} e.g. (51 + 77)\n\n > Choose an action: ").lower()
+                        if (_input == "single"):
+                            _input = -1
+                            while (_input > 10 or _input < 1):
+                                try:
+                                    _input = int(input(f"\n Number of Digits\n > How many digits should the numbers be?\n please {COLOR.WARNING}enter{COLOR.ENDC} a number of digits {COLOR.WARNING}between 1 and 10{COLOR.ENDC}: "))
+                                    if (_input >= 1 and _input <= 10):
+                                        settings['addition_settings']['number_of_digits'][0] = _input
+                                        break
+                                    else:
+                                        print(f"{COLOR.FAIL}Oops!{COLOR.ENDC} The number of digits you entered should be {COLOR.FAIL}between 1 and 10{COLOR.ENDC}!")
+                                except ValueError:
+                                    print(f"{COLOR.FAIL}Oops!{COLOR.ENDC} You should enter an {COLOR.FAIL}INTEGER{COLOR.ENDC}!")
+                            break
+                        if (_input == "interval -same"):
+                            input(" > interval -same")
+                            break
+                        if (_input == "interval -different"):
+                            input(" > interval -different")
+                            break
+                        os.system('cls')
+                        print("\n\n  + ADDITION SETTINGS \n\n")
+                        print(f" {COLOR.OKBLUE}[1]{COLOR.ENDC} Number of questions per run: {COLOR.WARNING}{settings['addition_settings']['questions_count']}{COLOR.ENDC}       (1...20)")
+                        print(f" {COLOR.OKBLUE}[2]{COLOR.ENDC} Number of digits: {COLOR.WARNING}{number_of_digits_settings_text}{COLOR.ENDC}(single/interval) -[same/different]")
+                        print(f"     {COLOR.WARNING}{how_many_digits_settings_text} digits{COLOR.ENDC} e.g. {example_digits_settings_text}")
+                        print(f" {COLOR.OKBLUE}[3]{COLOR.ENDC} Memorize Mode: {COLOR.WARNING}{memorize_mode_settings_text}{COLOR.ENDC}                    (on/off)")
+                        if (settings['addition_settings']['memorize_mode']):
+                            print(f"     {COLOR.WARNING}{settings['addition_settings']['memorize_seconds']} sec{COLOR.ENDC}")
+                        print(f" {COLOR.OKBLUE}[4]{COLOR.ENDC} Cumulative Mode: {COLOR.WARNING}{cumulative_mode_settings_text}{COLOR.ENDC}                  (on/off)")
+                elif (prompt == "3"):
+                    input(" > 3")
+                elif (prompt == "4"):
+                    input(" > 4")   
+                with open('config.yaml', 'w') as file:
+                    yaml.dump(settings, file)
         elif (prompt == "p"):
             os.system('cls')
             print("pathway")
