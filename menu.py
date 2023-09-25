0d882416cb9b660e58b1185fe8f9675e2b3062e4
Ege <53268284+Egosdev@users.noreply.github.com>
2023-09-25 23:36:54 +0300

    Localization tries
    colored yaml en_EN tr_TR

--- menu.py
+++ menu.py
@@ -96,6 +96,38 @@ def change_screen(i):
         
 change_screen(0)
 
+def display_settings(settings):
+    if (settings['addition_settings']['is_digit_interval']):
+        number_of_digits_settings_text = "Interval "
+        how_many_digits_settings_text = f"{settings['addition_settings']['number_of_digits'][0]}...{settings['addition_settings']['number_of_digits'][1]}"
+        if (settings['addition_settings']['is_digit_different']):
+            number_of_digits_settings_text += "-different "
+            example_digits_settings_text = "(** + ***), (**** + *) etc."
+        else:
+            number_of_digits_settings_text += "-same      "
+            example_digits_settings_text = "(** + **), (*** + ***) etc."
+    else:
+        number_of_digits_settings_text = "Single Value        "
+        how_many_digits_settings_text = f"{settings['addition_settings']['number_of_digits'][0]}"
+        example_digits_settings_text = "("+(settings['addition_settings']['number_of_digits'][0] * "*") + " + " + (settings['addition_settings']['number_of_digits'][0] * "*")+")"
+    if (settings['addition_settings']['memorize_mode']):
+        memorize_mode_settings_text = "on "
+    else:
+        memorize_mode_settings_text = "off"
+    if (settings['addition_settings']['cumulative_mode']):
+        cumulative_mode_settings_text = "on "
+    else:
+        cumulative_mode_settings_text = "off"
+    os.system('cls')
+    print("\n\n  + ADDITION SETTINGS \n\n")
+    print(f" {COLOR.OKBLUE}[1]{COLOR.ENDC} Number of questions per run: {COLOR.WARNING}{settings['addition_settings']['questions_count']}{COLOR.ENDC}       (1...20)")
+    print(f" {COLOR.OKBLUE}[2]{COLOR.ENDC} Number of digits: {COLOR.WARNING}{number_of_digits_settings_text}{COLOR.ENDC}(single/interval) -[same/different]")
+    print(f"     {COLOR.WARNING}{how_many_digits_settings_text} digits{COLOR.ENDC} e.g. {example_digits_settings_text}")
+    print(f" {COLOR.OKBLUE}[3]{COLOR.ENDC} Memorize Mode: {COLOR.WARNING}{memorize_mode_settings_text}{COLOR.ENDC}                    (on/off)")
+    if (settings['addition_settings']['memorize_mode']):
+        print(f"     {COLOR.WARNING}{settings['addition_settings']['memorize_seconds']} sec{COLOR.ENDC}")
+    print(f" {COLOR.OKBLUE}[4]{COLOR.ENDC} Cumulative Mode: {COLOR.WARNING}{cumulative_mode_settings_text}{COLOR.ENDC}                  (on/off)")
+
 while True:
     if (DISPLAY_SCREEN == 0):
         prompt = input(f" {COLOR.HEADER}>|{COLOR.ENDC} Type {COLOR.OKGREEN}[1-4]{COLOR.ENDC} to {COLOR.OKGREEN}select quiz{COLOR.ENDC}, {COLOR.FAIL}[9] {COLOR.ENDC}to {COLOR.FAIL}exit{COLOR.ENDC}: ")
@@ -140,49 +172,25 @@ while True:
             with open('config.yaml') as file:
                 settings = yaml.safe_load(file)
             while (prompt != "b"):
-                if (settings['addition_settings']['is_digit_interval']):
-                    number_of_digits_settings_text = "Interval "
-                    how_many_digits_settings_text = f"{settings['addition_settings']['number_of_digits'][0]}...{settings['addition_settings']['number_of_digits'][1]}"
-                    if (settings['addition_settings']['is_digit_different']):
-                        number_of_digits_settings_text += "-different "
-                        example_digits_settings_text = "(** + ***), (**** + *) etc."
-                    else:
-                        number_of_digits_settings_text += "-same      "
-                        example_digits_settings_text = "(** + **), (*** + ***) etc."
-                else:
-                    number_of_digits_settings_text = "Single Value        "
-                    how_many_digits_settings_text = f"{settings['addition_settings']['number_of_digits'][0]}"
-                    example_digits_settings_text = "("+(settings['addition_settings']['number_of_digits'][0] * "*") + " + " + (settings['addition_settings']['number_of_digits'][0] * "*")+")"
-                if (settings['addition_settings']['memorize_mode']):
-                    memorize_mode_settings_text = "on "
-                else:
-                    memorize_mode_settings_text = "off"
-                if (settings['addition_settings']['cumulative_mode']):
-                    cumulative_mode_settings_text = "on "
-                else:
-                    cumulative_mode_settings_text = "off"
-                os.system('cls')
-                print("\n\n  + ADDITION SETTINGS \n\n")
-                print(f" {COLOR.OKBLUE}[1]{COLOR.ENDC} Number of questions per run: {COLOR.WARNING}{settings['addition_settings']['questions_count']}{COLOR.ENDC}       (1...20)")
-                print(f" {COLOR.OKBLUE}[2]{COLOR.ENDC} Number of digits: {COLOR.WARNING}{number_of_digits_settings_text}{COLOR.ENDC}(single/interval) -[same/different]")
-                print(f"     {COLOR.WARNING}{how_many_digits_settings_text} digits{COLOR.ENDC} e.g. {example_digits_settings_text}")
-                print(f" {COLOR.OKBLUE}[3]{COLOR.ENDC} Memorize Mode: {COLOR.WARNING}{memorize_mode_settings_text}{COLOR.ENDC}                    (on/off)")
-                if (settings['addition_settings']['memorize_mode']):
-                    print(f"     {COLOR.WARNING}{settings['addition_settings']['memorize_seconds']} sec{COLOR.ENDC}")
-                print(f" {COLOR.OKBLUE}[4]{COLOR.ENDC} Cumulative Mode: {COLOR.WARNING}{cumulative_mode_settings_text}{COLOR.ENDC}                  (on/off)")
+                display_settings(settings)
                 prompt = input(f"\n {COLOR.HEADER}>|{COLOR.ENDC} {COLOR.FAIL}b{COLOR.ENDC} or {COLOR.FAIL}B{COLOR.ENDC} to {COLOR.FAIL}back{COLOR.ENDC}, {COLOR.OKGREEN}[1-4]{COLOR.ENDC} to {COLOR.OKGREEN}select an action{COLOR.ENDC} for more info and adjustments: ").lower()
                 if (prompt == "1"):
                     _input = -1
                     while (_input > 20 or _input < 1):
                         try:
-                            _input = int(input(f"\n Number of Questions\n > To specify how many consecutive questions will be asked,\n please {COLOR.WARNING}enter{COLOR.ENDC} a number {COLOR.WARNING}between 1 and 20{COLOR.ENDC}: "))
+                            _input = input(f"\n Number of Questions\n **Current number of questions per run: {COLOR.WARNING}{settings['addition_settings']['questions_count']}{COLOR.ENDC}\n > To specify how many consecutive questions will be asked,\n please {COLOR.WARNING}enter{COLOR.ENDC} a number {COLOR.WARNING}between 1 and 20{COLOR.ENDC}: ")
+                            if (_input == 'b' or _input == 'B'): break
+                            _input = int(_input)
                             if (_input >= 1 and _input <= 20):
                                 settings['addition_settings']['questions_count'] = _input
                                 break
                             else:
-                                print(f"{COLOR.FAIL}Oops!{COLOR.ENDC} The number you entered should be {COLOR.FAIL}between 1 and 20{COLOR.ENDC}!\n")
+                                os.system('cls')
+                                print(f"\n\n{COLOR.FAIL} Oops!{COLOR.ENDC} The number you entered should be {COLOR.FAIL}between 1 and 20{COLOR.ENDC}!\n")
                         except ValueError:
-                            print(f"{COLOR.FAIL}Oops!{COLOR.ENDC} You should enter an {COLOR.FAIL}INTEGER{COLOR.ENDC}!\n")
+                            #os.system('cls')
+                            break
+                            print(f"\n\n{COLOR.FAIL} Oops!{COLOR.ENDC} You should enter an {COLOR.FAIL}INTEGER{COLOR.ENDC}!\n")
                 elif (prompt == "2"):
                     _input = ""
                     while (_input != "single" or _input != "interval -same" or _input != "interval -different"):
